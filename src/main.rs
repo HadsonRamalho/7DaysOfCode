ed9ae9364124daf801133a2ee04cc19e67a5c5fe
Hadson <hadsonramalho@gmail.com>
2024-03-11 17:35:09 -0300

    Corrigindo tipos de retorno das funções adicionar e listar

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 use std::io;
-use std::io::{BufRead, Read};
 
 #[derive(Clone, Default, Debug)]
-struct item{
+struct Item{
     nome:String
 }
 
 #[derive(Copy, Clone, Default, Debug)]
-struct quantidade{
+struct Quantidade{
     qtd:u32
 }
 
 #[derive(Default)]
 struct ListaDeCompra{
-    qtds:[quantidade; 10], itens:[item; 10]
+    qtds:[Quantidade; 10], itens:[Item; 10]
 }
 
 fn stou(y:String) -> u32{
@@ -24,23 +23,22 @@ fn stou(y:String) -> u32{
     return y
 }
 
-fn adicionar_item(mut lista:ListaDeCompra, nome_param:String, qtd_param:u32, mut qtd_itens:usize) -> ListaDeCompra{
-    let mut item:item = Default::default();
+fn adicionar_item(mut lista:ListaDeCompra, nome_param:String, qtd_param:u32,mut qtd_itens:usize) -> (ListaDeCompra, u32){
+    let mut item:Item = Default::default();
     item.nome = nome_param;
     lista.itens[qtd_itens] = item;
-    let it = &lista.itens[qtd_itens];
-    let mut qtd:quantidade = Default::default();
+    let mut qtd:Quantidade = Default::default();
     qtd.qtd = qtd_param;
     lista.qtds[qtd_itens] = qtd;
-    //println!("Nome do item adicionado: {}", it.nome);
-    //println!("Quantidade do item adicionado: {}", qtd.qtd);
-    qtd_itens += 1;
-    lista
+    let num= qtd_itens.to_string();
+    let num = stou(num);
+    let t: (ListaDeCompra, u32) = (lista, num);
+    t
 }
 
-fn listar_itens(mut lista:ListaDeCompra, qtd_itens:usize){
-    let mut item = &lista.itens;
-    let mut quantidades = &lista.qtds;
+fn listar_itens(lista:ListaDeCompra, mut qtd_itens: u32) -> ListaDeCompra{
+    let item = &lista.itens;
+    let quantidades = &lista.qtds;
     let mut tam = 0;
     for i in item{
         if tam <= qtd_itens {
@@ -55,13 +53,15 @@ fn listar_itens(mut lista:ListaDeCompra, qtd_itens:usize){
         }
         tam += 1;
     }
+    qtd_itens+=1;
+    lista
 }
 
 fn main() {
     let mut lista:ListaDeCompra = Default::default();//
     let mut nome_item:String = Default::default();
     let mut qtd_item:String = Default::default();
-    let mut qtd_itens:usize = 0;
+    let mut qtd_itens:u32 = 0;
     println!(" | Adicionando itens na lista");
     println!(" | Insira o nome do item: ");
     io::stdin().read_line(&mut nome_item).expect(" > Erro ao ler o nome do item");
@@ -69,7 +69,15 @@ fn main() {
     io::stdin().read_line(&mut qtd_item).expect(" > Erro ao ler a quantidade do item");
     let qtd_item = stou(qtd_item);
     println!(" | Listando itens da lista");
-    lista = adicionar_item(lista, nome_item, qtd_item, qtd_itens);
-    listar_itens(lista, qtd_itens);
+    let tupla_lista_qtd_nome:(ListaDeCompra, u32);
+    tupla_lista_qtd_nome =
+        adicionar_item(lista, nome_item, qtd_item, qtd_itens as usize);
+    lista =
+        tupla_lista_qtd_nome.0;
+    qtd_itens =
+        tupla_lista_qtd_nome.1 as usize as u32;
+    lista =
+        listar_itens(lista, qtd_itens);
+
     println!("OK");
 }
